59826d1bcde85bfd3b716fb69b7e3d7be40371ee
SalihEris <150025482+SalihEris@users.noreply.github.com>
2025-07-01 13:14:57 +0100

    fixed

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,6 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AllergyController;
-
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,10 +16,5 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-
-Route::get('/gezinsallergie-overzicht', [AllergyController::class, 'index'])->name('allergy.overview');
-Route::get('/gezinsallergie-overzicht/filter', [AllergyController::class, 'filter'])->name('allergy.overview.filter');
-
 
 require __DIR__.'/auth.php';
