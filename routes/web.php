8a689f5f1812bc1769010723f2903d064e620291
mo121213132131 <hamoudishaaban16@gmail.com>
2025-07-01 13:11:57 +0200

    update customers to 1 page file

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClientController;
-
-
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,27 +13,28 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// Profile routes
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-
+// Admin routes
 Route::middleware(['auth', 'role:admin'])->prefix('admin')->name('admin.')->group(function () {
     Route::resource('users', UserController::class);
 });
 
+// Shared customer routes for all roles
 Route::middleware('auth')->group(function () {
-
-    // Klantenoverzicht
+    // Customer overview and filtering
     Route::get('/clients', [ClientController::class, 'index'])->name('clients.index');
     Route::post('/clients/filter', [ClientController::class, 'filter'])->name('clients.filter');
 
-    // Klantdetails
+    // View customer details
     Route::get('/clients/{id}', [ClientController::class, 'show'])->name('clients.show');
 
-    // Klant bewerken
+    // Edit customer
     Route::get('/clients/{id}/edit', [ClientController::class, 'edit'])->name('clients.edit');
     Route::put('/clients/{id}', [ClientController::class, 'update'])->name('clients.update');
 });
